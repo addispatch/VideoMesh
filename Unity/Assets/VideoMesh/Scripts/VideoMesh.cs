1d5fd343e45ed888fa87716beb17aad7646f9b74
Christopher Baltzer <cbaltzer@gmail.com>
2013-10-29 22:58:12 -0300

    unity side stubbed

--- Unity/Assets/VideoMesh/Scripts/VideoMesh.cs
+++ Unity/Assets/VideoMesh/Scripts/VideoMesh.cs
@@ -7,16 +7,35 @@ public class VideoMesh : MonoBehaviour {
 	void Start () {
 		// not necessary now, but we'll use it later
 	}
-	
+
+
+	[DllImport("__Internal")]
+	private static extern void _setVideo(string filename);
+
 	public void SetVideo(string filename) {
+		if (Application.platform == RuntimePlatform.IPhonePlayer) {
+			_setVideo(filename);
+		}
 	}
 
+
+	[DllImport("__Internal")]
+	private static extern void _play();
 
 	public void Play() {
+		if (Application.platform == RuntimePlatform.IPhonePlayer) {
+			_play();
+		}
 	}
 
 
+	[DllImport("__Internal")]
+	private static extern void _pause();
+
 	public void Pause() {
+		if (Application.platform == RuntimePlatform.IPhonePlayer) {
+			_pause();
+		}
 	}
 
 }
