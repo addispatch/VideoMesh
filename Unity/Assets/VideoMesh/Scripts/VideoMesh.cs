b62ecf9c74b6c9ba5750c31edecc6e5da0b3cccc
Christopher Baltzer <cbaltzer@gmail.com>
2013-10-29 22:39:02 -0300

    started script

---
+++ Unity/Assets/VideoMesh/Scripts/VideoMesh.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices; // Don't forget this!
+
+public class VideoMesh : MonoBehaviour {
+	
+	void Start () {
+		// not necessary now, but we'll use it later
+	}
+	
+	public void SetVideo(string filename) {
+	}
+
+
+	public void Play() {
+	}
+
+
+	public void Pause() {
+	}
+
+}
